ff97bf7356d3ada49ec5933a74211759ee20bab2
seonghyeon.min <seonghyeon.min@lge.com>
2024-08-23 13:34:22 +0900

    Test

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("New line");
+        System.out.print("");
+        System.out.print("");
+        System.out.println("b");
     }
 }
